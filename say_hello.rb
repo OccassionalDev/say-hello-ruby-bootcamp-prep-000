816561938e0d85e98b505610573fbf1023cef182
active-class-0481 <bestlittleangel1@gmail.com>
2019-12-12 04:35:01 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
 
-def say_hello(name = "Ruby Programmer", langauage = "Ruby")
-  puts "Hello #{name}. We heard you are a great #{language} programmer."
+def say_hello(name = "Ruby Programmer")
+  puts "Hello #{name}!"
 end
